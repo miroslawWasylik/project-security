be9d1130ae23327ec2b1e321c646c6d3f9e38435
ladenisko <ladenmail@gmail.com>
2024-06-19 00:01:22 +0200

    add ssl certification and install helmet

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
+const fs = require('fs');
+const https = require('https');
 const path = require('path');
 const express = require('express');
+const helmet = require('helmet');
 
 const PORT = 3000;
 
 const app = express();
+
+app.use(helmet());
 
 app.get('/secret', (req, res) => {
 	return res.send('Your personal secret value is 42!');
@@ -13,6 +18,9 @@ app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
+https.createServer({
+	key: fs.readFileSync('key.pem'),
+	cert: fs.readFileSync('cert.pem'),
+}, app).listen(PORT, () => {
 	console.log(`listening on port ${PORT}`);
-})
+});

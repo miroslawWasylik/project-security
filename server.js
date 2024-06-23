073a61a024314cc7173abb4720396eba23869493
ladenisko <ladenmail@gmail.com>
2024-06-23 02:57:51 +0200

    login with sessions

--- server.js
+++ server.js
@@ -72,7 +72,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 function checkLoggedIn(req, res, next) {
-	const isLoggedIn = true; //TODO
+	console.log('Current user', req.user);
+	const isLoggedIn = req.user; //TODO
 	if(!isLoggedIn) {
 		return res.status(401).json({
 			error: 'You must log in!'
